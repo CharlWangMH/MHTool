181cac53c47dc93beb0cdae6202417e32be99949
王朝 <charlwang@126.com>
2020-05-10 18:19:15 +0800

    s

---
+++ MHTOOL/Classes/MHPerson.h
@@ -0,0 +1,17 @@
+//
+//  MHPerson.h
+//  MHTOOL_Example
+//
+//  Created by admin on 2020/5/10.
+//  Copyright © 2020 CharlWangMH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MHPerson : NSObject
++(void)show;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MHTOOL/Classes/MHPerson.m
@@ -0,0 +1,15 @@
+//
+//  MHPerson.m
+//  MHTOOL_Example
+//
+//  Created by admin on 2020/5/10.
+//  Copyright © 2020 CharlWangMH. All rights reserved.
+//
+
+#import "MHPerson.h"
+
+@implementation MHPerson
++(void)show {
+    NSLog(@"为人处世类似程序设计, 尽量暴露出简洁高效的API,同时少打印Log");
+}
+@end
